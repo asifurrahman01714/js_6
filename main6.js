0ab5fd0d4d01d9201f806337158b5206099eca05
Md.Asif Ur Rahman <45636891+asifurrahman01714@users.noreply.github.com>
2020-09-13 19:35:03 +0600

    Add files via upload

--- main6.js
+++ main6.js
@@ -1,5 +1,6 @@
 
 var letter=prompt("enter the letter: ")  ;
+letter=letter.toLowerCase();
 
 if(letter=="a" || letter=="e" ||letter=="i" || letter=="o"||letter=="u")
 document.write("The letter is vowel ");
