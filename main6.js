02bf593b22b0e5fa2a734d50d8460a77db3e280c
Md.Asif Ur Rahman <45636891+asifurrahman01714@users.noreply.github.com>
2020-09-13 19:07:18 +0600

    Add files via upload

---
+++ main6.js
@@ -0,0 +1,8 @@
+
+var letter=prompt("enter the letter: ")  ;
+
+if(letter=="a" || letter=="e" ||letter=="i" || letter=="o"||letter=="u")
+document.write("The letter is vowel ");
+else
+document.write("The letter is consonant");
+
